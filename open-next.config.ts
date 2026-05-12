f4329d94884101992327a3992a7d4f589fb6afdd
rashadatjuned-alt <rashadat.juned@gmail.com>
2026-05-12 17:53:10 +0600

    Update open-next.config.ts

--- open-next.config.ts
+++ open-next.config.ts
@@ -2,7 +2,6 @@ import type { OpenNextConfig } from "@opennextjs/cloudflare";
 
 const config: OpenNextConfig = {
   default: {
-    // 'smart' placement allows Cloudflare to decide where to run code for best speed
     placement: "smart",
   },
 };
