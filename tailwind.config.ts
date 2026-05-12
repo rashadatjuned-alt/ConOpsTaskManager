4db9477fd4bf3165d98a9170663ab68432917116
rashadatjuned-alt <rashadat.juned@gmail.com>
2026-05-12 17:33:41 +0600

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,19 @@
-import type { Config } from 'tailwindcss'
-const config: Config = {
-  content: ['./pages/**/*.{js,ts,jsx,tsx,mdx}','./components/**/*.{js,ts,jsx,tsx,mdx}','./app/**/*.{js,ts,jsx,tsx,mdx}'],
-  theme: { extend: {} },
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./lib/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        // You can add your custom variables here if needed
+        accent: "var(--accent)",
+        accent2: "var(--accent2)",
+      },
+    },
+  },
   plugins: [],
-}
-export default config
+};
